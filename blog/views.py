b081114d150b34ffe0dfab587bf495893b15e83d
Oksana Niklashkova <o.niklashkova@gmail.com>
2025-08-06 20:27:31 +0300

    develop blog-app

--- blog/views.py
+++ blog/views.py
@@ -16,11 +16,20 @@ class PublicationListView(ListView):
     template_name = 'blog/publication_list.html'
     context_object_name = 'publications'
 
+    def get_queryset(self):
+        return Publication.objects.filter(is_publicated=True)
+
 
 class PublicationDetailView(DetailView):
     model = Publication
     template_name = 'blog/publication_detail.html'
     context_object_name = 'publication'
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_counter += 1
+        self.object.save()
+        return self.object
 
 
 class PublicationUpdateView(UpdateView):
@@ -28,6 +37,9 @@ class PublicationUpdateView(UpdateView):
     fields = ['heading', 'text', 'preview']
     template_name = 'blog/publication_create.html'
     success_url = reverse_lazy('blog:publication_list')
+
+    def get_success_url(self):
+        return reverse_lazy('blog:publication_detail', args=[self.kwargs.get('pk')])
 
 
 class PublicationDeleteView(DeleteView):

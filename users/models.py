1e241722e5e62b2d12098f5595fe4b36b0ec88de
Oksana Niklashkova <o.niklashkova@gmail.com>
2025-08-14 19:52:30 +0300

    init post-service

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     """класс, определяющий пользователя приложения"""
+    username = models.CharField(max_length=50, blank=True, null=True, unique=False)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15, verbose_name='Телефон', blank=True, null=True)
     country = models.CharField(max_length=50, verbose_name='Страна', blank=True, null=True)

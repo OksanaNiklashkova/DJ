238b69470b9437c4cefced9ecd54ed9e1a2cd222
Oksana Niklashkova <o.niklashkova@gmail.com>
2025-08-16 14:25:20 +0300

    assignment of rights for moderators

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
-from django.views.generic import ListView, TemplateView, DetailView, DeleteView
+from django.views.generic import ListView, TemplateView, DetailView, DeleteView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseForbidden
 
 from .forms import ProductForm
 from .models import Product
 
+class ProductUnpublishedView(LoginRequiredMixin, View):
+    model = Product
+
+    def post(self, request, product_id):
+        product = get_object_or_404(Product, product_id)
+        if not self.request.user.has_perm('catalog.can_unpublished_product'):
+            return HttpResponseForbidden('У вас нет прав для отмены публикации продукта')
+        product.is_published = False
+        product.save()
+        redirect('catalog:product_card', pk=product_id)
 
 class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
@@ -15,7 +26,7 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy('catalog:home')
 
     def form_valid(self, form):
-        form.instance.user = self.request.user
+        form.instance.owner = self.request.user
         return super().form_valid(form)
 
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
@@ -47,6 +58,14 @@ class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     template_name = 'catalog/product_confirm_delete.html'
     success_url = reverse_lazy('catalog:home')
+
+    # def post(self, request, form, product_id=id):
+    #     product = get_object_or_404(Product, product_id)
+    #     if not self.request.user.has_perm('catalog.can_delete_product'):
+    #         return HttpResponseForbidden('У вас нет прав для удаления продукта')
+    #     else:
+    #         form.instance.user = self.request.user
+    #         return super().form_valid(form)
 
     def form_valid(self, form):
         form.instance.user = self.request.user
